db20d23a35c980d43d12460f0e18aebfdfc9281a
ad3bay0 <byoextreme@hotmail.com>
2020-03-24 21:48:01 +0100

    chore(csv): csv data parser

--- src/main/java/com/ad3bay0/covid19tracker/services/ApiService.java
+++ src/main/java/com/ad3bay0/covid19tracker/services/ApiService.java
@@ -1,32 +1,42 @@
 package com.ad3bay0.covid19tracker.services;
 
+import java.io.IOException;
+import java.io.StringReader;
+
 import javax.annotation.PostConstruct;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * ApiService
  */
-@Component
+@Service
 public class ApiService {
 
     private static String DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-23-2020.csv";
     private static Logger log = LoggerFactory.getLogger(ApiService.class);
 
     @PostConstruct
-    public void fetchData(){
+    public void fetchData() throws IOException {
 
         RestTemplate restTemplate = new RestTemplate();
         ResponseEntity<String> response = restTemplate.getForEntity(DATA_URL, String.class);
-        log.info(response.getBody());
 
+        StringReader in = new StringReader(response.getBody());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
+        for (CSVRecord record : records) {
+            String state = record.get("Province_State");
+            log.info(state);
 
+        }
+        
 
     }
 
-    
 }
